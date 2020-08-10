faf580cdbcec2e1700667b1855f13e5d4e71de54
Jon Weatherhead <jon@planetjon.ca>
2020-08-10 13:07:21 -0400

    showwiz backports. less bloat is always good.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ) ?>">
 	<label for="s" class="assistive">Search for: </label>
-	<input type="search" placeholder="&#8981;&#8230;" size="14" value="<?php the_search_query() ?>" name="s" id="s"/>
+	<input type="search" placeholder="&#8981;" size="14" value="<?php the_search_query() ?>" name="s" id="s"/>
 </form>
